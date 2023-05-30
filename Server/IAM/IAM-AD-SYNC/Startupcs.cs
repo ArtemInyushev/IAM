c92b5154ae08f00913c5cadcc749c283603d98c6
ArtemInyushev <art10inyushev@gmail.com>
2023-05-30 16:38:31 +0300

    Add Function app

---
+++ Server/IAM/IAM-AD-SYNC/Startupcs.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Function1
+{
+    public class Startup : IWebJobsStartup
+    {
+        public void Configure(IWebJobsBuilder builder)
+        {
+            // Configure your services and settings here
+            ConfigureServices(builder.Services);
+        }
+
+        private void ConfigureServices(IServiceCollection services)
+        {
+            // Add your services to the service collection
+            // For example:
+            // services.AddTransient<ISomeService, SomeService>();
+        }
+    }
+}
